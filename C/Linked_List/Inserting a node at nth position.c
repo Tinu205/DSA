3767641c2626c1d9d303ed9c360d546ac7ccf11d
Tinu205 <dazleroger404@gmail.com>
2023-10-10 17:18:04 +0530

    Modified inserting at nth position since the previos one didn't work as intented and other files

--- C/Linked_List/Inserting a node at nth position.c
+++ C/Linked_List/Inserting a node at nth position.c
@@ -12,32 +12,32 @@ void insert(int val,int pos){
   struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
   temp->data = val;
   temp->next = NULL;
-  if(pos == 1){
+  if(val == 1){//inserts at the beginning even if its null;
     temp->next = head;
     head = temp;
-  }else{
-    struct Node* temp2 = head;
-    for(int i=0;i<pos-2;i++){
-      temp2 = temp2->next;
-    }
-    temp->next = temp2->next;
-    temp2->next = temp;
+    return;
   }
+  struct Node* temp2 = head;
+  for(int i=0;i<pos-2;i++){//loop till n-2 to stop at n-1th element
+    temp2 = temp2->next;
+  }
+  temp->next = temp2->next;
+  temp2->next = temp;
 }
 
 void print(){
- struct Node* temp = head;  
-  while(temp->next != NULL){
-    printf("%d",temp->data);
-    temp = temp->next;
+  struct Node* current=head;
+  while(current != NULL){
+    printf(" (%p)%d->%p",&(current->data),current->data,current->next);
+    current=current->next;
+    printf("\n");
   }
-   printf("\n");
 }
-
 void main(){
   head = NULL;
   insert(1,1);
   insert(2,2);
   insert(3,3);
+  insert(4,4);
   print();
 }
