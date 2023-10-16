bc9ef65290b2e2c5ed1ad2157cfd0d9dc112047c
Tinu205 <dazleroger404@gmail.com>
2023-10-16 18:34:41 +0530

    NOt completed yet

--- C/Linked_List/Removing element at nth place.c
+++ C/Linked_List/Removing element at nth place.c
@@ -10,18 +10,18 @@
 struct Node{
   int data;
   struct Node* next;
-}
+};
 
-struct Node head;
+struct Node* head;
 
 void append(int val){
-  struct Node temp1  = (struct Node*)malloc(sizeof(struct Node));
+  struct Node* temp1  = (struct Node*)malloc(sizeof(struct Node));
   temp1->data = val;
   temp1->next = NULL;
   if(head == NULL){
     head = temp1;
   }else{
-    struct Node temp2 = head;
+    struct Node* temp2 = head;
     while(temp2->next != NULL){
       temp2 = temp2->next;
     }
@@ -30,9 +30,31 @@ void append(int val){
 }
 
 void print(){
-  struct Node current=head;
-  while
+  struct Node* current=head;
+  while(current->next != NULL){
+    printf("%d ",current->data);
+    current = current->next;
+  }
+}
+
+void rem(int val){
+  struct Node* temp = head;
+  for(int i = 0; i<val-2;i++){
+    temp = temp->next;      
+  }
+  struct Node* temp2=temp->next;
+  temp->next = temp->next;
+  free(temp2);
+   
 }
 void main(){
   head = NULL;
+  append(0);
+  append(1);
+  append(2);
+  append(3);
+  append(4);
+  print();
+  rem(3);
+  print();
 }
