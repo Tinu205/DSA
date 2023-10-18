62d5f1d7c717be4d97690fef3b58f83ab1ff4e42
Tinu205 <dazleroger404@gmail.com>
2023-10-18 15:26:57 +0530

    Added features

---
+++ C/Linked_List/Reversing an linked list.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node{
+  int data;
+  struct Node* next;
+};
+
+struct Node head;
+
+void insert(int val){
+  struct Node* temp = (struct Node*)malloc(sizeof)
+}
+
+void reverse(){
+
+}
+
+void main(){
+  head = NULL;
+  insert(0);
+  insert(1);
+  insert(2);
+  insert(3);
+  insert(4);
+  insert(5);
+  reverse();
+}
