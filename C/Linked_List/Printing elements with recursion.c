f968b61d5523ac7acc06b8f11ee3ad171c412540
Tinu205 <dazleroger404@gmail.com>
2023-11-03 21:21:23 +0530

    Finally worked

--- C/Linked_List/Printing elements with recursion.c
+++ C/Linked_List/Printing elements with recursion.c
@@ -6,36 +6,82 @@ struct Node{
   struct Node* next;
 };
 
-struct Node* head = NULL;
+struct Node* head;
 
-//inserting an element at the end
-void insert(int data){
-  struct Node* temp = (struct Node *)malloc(sizeof(struct Node));
-  temp->data = data;
+//Inserting elements at the end
+void insert(int a){
+  struct Node* temp = (struct Node*)(malloc(sizeof(struct Node)));
+  temp->data = a;
   temp->next = NULL;
-  if(head = NULL){
+  if(head == NULL){
     head = temp;
     return;
   }
-  struct Node* current = head;
-  while(current != NULL){
-    current = current->next;
-  }
-  current->next = temp;
+   struct Node* current = head;
+   while(current->next != NULL){
+     current = current->next;
+   }
+   current->next = temp;
 }
 
-//Recursion for printing normally
-void normal(struct Node *head){
-  if(head == NULL){
+//Printing the elements in order
+void normal(struct Node* val){
+  struct Node* temp = val;
+  if(temp == NULL){
+    printf("\nEnd of the linked list");
     return;
   }
-  printf("%d",head->data);
-  normal(head->next);
+  printf("%d ",temp->data);
+  normal(temp->next);
 }
 
-void main(){
-  for(int i=0;i<5;i++){
-    insert(i+1);
+//Printing in reverse elements in order
+void reverse(struct Node* val){
+  struct Node* temp = val;
+  if(temp == NULL){
+    return;
   }
-  normal(head);
+ 
+  reverse(temp->next);
+  printf("%d ",temp->data);
 }
+void main(){
+  head = NULL;
+  insert(1);
+  insert(2);
+  insert(3);
+  printf("Printing linked list in normal order: ");
+  normal(head);
+  printf("\nPrinting the linked list in reverse order: ");
+  reverse(head);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
