d9d459cffb4135cb25585727e4d58922f922a07a
Tinu205 <dazleroger404@gmail.com>
2023-11-06 17:23:48 +0530

    Added insert and print for reverse recursion

---
+++ C/Linked_List/Reversing a linked list using recursion.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
+  int data;
+  struct Node* next;
+};
+
+struct Node* head;
+
+void insert(int val){
+  struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+  temp->data = val;
+  temp->next = NULL;
+  if(head == NULL){
+    head = temp;
+    printf("Head inserted\n");
+    return;
+  }
+  struct Node* temp2 = head;
+  while(temp2->next != NULL){
+    temp2 = temp2->next;
+    //printf("%d inserted\n",temp2->data);
+  }
+  temp2->next = temp;
+}
+
+void print(){
+  struct Node* temp = head;
+  while(temp->next != NULL){
+    printf("%d ",temp->data);
+    temp = temp->next;
+  }
+}
+void main(){
+  head = NULL;
+  insert(1);
+  insert(2);
+  insert(3);
+  insert(4);
+  insert(5);
+  print();
+}
