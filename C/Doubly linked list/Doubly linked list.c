b1dc188f9f6bf0394b4050b61c461bbd34318a38
Tinu205 <dazleroger404@gmail.com>
2023-11-19 18:05:17 +0530

    Added functional linked list

--- C/Doubly linked list/Doubly linked list.c
+++ C/Doubly linked list/Doubly linked list.c
@@ -10,6 +10,7 @@ struct node{
 struct node* head;
 
 struct node* getnewnode(int x){
+  //printf("Get newnode called");
   struct node* newnode = (struct node*)malloc(sizeof(struct node));
   newnode->data = x;
   newnode->next = NULL;
@@ -18,28 +19,64 @@ struct node* getnewnode(int x){
 }
 
 void insert_at_head(int x){
+  //printf("Inserted at the head called");
   struct node *newnode = getnewnode(x);
   if(head == NULL){
     head = newnode;
+    //printf("Inserted");
     return;
   }
   head->prev = newnode;
   newnode->next = head;
   head = newnode;  
+  //printf("Inserted");
+}
+
+void insert_at_tail(int x){
+  struct node* newnode = getnewnode(x);
+  struct node* temp = head;//to traverse
+  if(head == NULL){
+    printf("Empty linked list so added at the head \n");
+    head = newnode;
+    return;
+  }
+  while(temp->next != NULL){
+    temp = temp->next;
+  }
+  temp->next = newnode;
+  newnode->prev = temp;
 }
 
 void print(){
   struct node* temp = head;
-  while(temp->next != NULL);
+  while(temp != NULL)
   {
-    printf("%d",temp->data);
+    printf("%d ",temp->data);
     temp = temp->next;
   }
   printf("\n");
 }
 
+void reverse_print(){
+  struct node *temp = head;
+  while(temp != NULL && temp->next != NULL){
+    temp = temp->next;
+  }
+  while(temp != NULL){
+    printf("%d ",temp->data);
+    temp = temp->prev;
+  }
+  printf("\n");
+}
 void main(){
   head = NULL;
-  insert_at_head(1);
-  insert_at_head(1);  
+  for(int i=1;i<5;i++){
+    insert_at_tail(i);
+  }
+  insert_at_head(0);
+  printf("In oreder: ");
+  print();
+  printf("In reverse : ");
+  reverse_print();
 }
+
