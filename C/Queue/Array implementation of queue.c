d48afe674d9a66d8c97ce614b4d4cb6ab0733e96
Tinu205 <dazleroger404@gmail.com>
2023-11-24 17:59:50 +0530

    Queue as adt

---
+++ C/Queue/Array implementation of queue.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int A[10];
+int fornt = -1;
+int rear = -1;
+
+int isempty(){
+  if (front == -1 && rear == -1){
+    return 0;
+  }else{
+    return 1;
+  }
+}
+
+void enqueue(int x){
+  if(isempty()){
+    
+  }
+}
+
+void dequeue(){
+
+}
+
+void print(){
+
+}
+
+void main(){
+
+}
