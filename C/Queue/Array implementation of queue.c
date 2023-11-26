051c868a0e123d0fbe51ec901f0fd1829b757396
Tinu205 <dazleroger404@gmail.com>
2023-11-26 18:24:02 +0530

    Added Array implementation of queue

--- C/Queue/Array implementation of queue.c
+++ C/Queue/Array implementation of queue.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
+//Should follow fifo principle
 
-int A[10];
-int fornt = -1;
+#include<stdio.h>
+#define max 100
+
+int arr[max];
+int front = -1;
 int rear = -1;
 
 int isempty(){
@@ -13,19 +16,49 @@ int isempty(){
 }
 
 void enqueue(int x){
-  if(isempty()){
-    
+  if(rear+1<max){ 
+    if(!isempty()){
+      front ++;
+      rear ++;
+      arr[rear] = x;
+    }else{
+      rear++;
+      arr[rear] = x;
+    }
+  }else{
+    printf("Buffer overflow ");
   }
+
 }
 
 void dequeue(){
-
+  if(rear == front){
+    front = -1;
+    rear = front;
+  }else if(front == -1){
+    printf("bufferunderflow");
+  }else{
+    front+=1;
+  }
+  return;
 }
 
 void print(){
-
+  for(int i = front; i<=rear;i++){
+    printf("%d ",arr[i]);
+  }
+  return;
 }
 
 void main(){
-
+  for(int i = 0;i<5;i++){
+    enqueue(i+1);
+  }
+  print();
+  printf("\nDequeuing an element\n");
+  dequeue();
+  print();
+  printf("\nEnqueueing 6\n");
+  enqueue(6);
+  print();
 }
