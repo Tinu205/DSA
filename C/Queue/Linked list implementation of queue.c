773d3a20975c5742409eaf26f79f356c502ea9c4
Tinu205 <dazleroger404@gmail.com>
2023-11-27 18:19:36 +0530

    Added Linkedlist implementation of queue.c and it works

--- C/Queue/Linked list implementation of queue.c
+++ C/Queue/Linked list implementation of queue.c
@@ -10,13 +10,43 @@ struct Node* head = NULL;
 struct Node* tail = NULL;
 
 void print(){
-  struct node* temp = head;
-  while(temp->next != NULL){
-    printf("%d",temp->data);
+  struct Node* temp = head;
+  while(temp != NULL){
+    printf("%d ",temp->data);
     temp = temp->next;
   }
 }
 
-void main(){
+void enqueue(int x){
+  struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
+  temp->data = x;
+  temp->next = NULL;
+  if(head == NULL){
+    head = tail = temp;
+    return;
+  }else{
+    tail->next = temp;
+    tail = temp;
+  }
+}
 
+void dequeue(){
+  if(head == NULL){
+    printf("Empty queue");
+    return;
+  }
+  struct Node* temp = head;
+  head = head->next;
+  free(temp);
+  
+}
+
+void main(){
+  for(int i = 0;i<5;i++){
+    enqueue(i);
+  }
+  print();
+  dequeue();
+  printf("\nDequeuing an element: \n");
+  print();
 }
