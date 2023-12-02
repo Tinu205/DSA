4ae0b9d54283655b8145822bb4c4ebefe26343c5
Tinu205 <dazleroger404@gmail.com>
2023-12-02 18:41:53 +0530

    Added BST fully:

--- C/Binary search tree/BST implementation.c
+++ C/Binary search tree/BST implementation.c
@@ -10,8 +10,8 @@ struct Node{
 struct Node* getNewnode(int val){
   struct Node* root = (struct Node*)malloc(sizeof(struct Node));
   root->data =  val;
-  root ->left = NULL;
-  root -> right = NULL;
+  root->left = NULL;
+  root->right = NULL;
   return root;
 }
 
@@ -26,11 +26,29 @@ void insert(struct Node** root, int val){
   }
 }
 
+int search(struct Node** root,int val){
+  int value = 0;
+  if((*root)->data == val){
+    value = 1;
+  }else if((*root)->data > val){
+    value=search(&(*root)->left,val);
+  }else{
+    value=search(&(*root)->right, val);
+  }
+  return value;
+}
+
 void main(){
   struct Node* root = NULL;
   insert(&root,5);
   insert(&root,6);
   insert(&root,4);
-  struct Node* temp = root->left;
-  printf("%d",temp->data);
+  //struct Node* temp = root->left;
+  //printf("%d",temp->data);
+  int result = search(&root,4);
+  if(result == 1){
+    printf("Element found");
+  }else{
+    printf("Element not found");
+  }
 }
