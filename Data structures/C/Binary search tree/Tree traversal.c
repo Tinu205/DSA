653947b9be02d4ed2978c3a0ff6f68157d45921a
Tinu205 <dazleroger404@gmail.com>
2023-12-21 21:53:51 +0530

    Added Tree traversal.c

---
+++ Data structures/C/Binary search tree/Tree traversal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+  int data;
+  struct node* left = NULL;
+  struct node* right = NULL;
+};
+
+void insert(**root,int data){
+
+}
+void main(){
+  struct node* root = NULL;
+}
